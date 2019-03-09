935b3b63670535c0f3ad3aebf81323e3f5dc6bef
Jacob Murray <jaymur2010@gmail.com>
2019-03-09 01:55:46 -0500

    Menu Fonctionnel

---
+++ GamejamRimouski/Assets/Scenes/Script/Menu/MainMenuController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+
+public class MainMenuController : MonoBehaviour
+{
+    public GameObject creditMenu;
+    public GameObject mainMenu;
+    public EventSystem eventSystem;
+    public GameObject returnButton;
+    public GameObject playButton;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MainScene", LoadSceneMode.Single);
+    }
+    public void Credits()
+    {
+        mainMenu.SetActive(false);
+        creditMenu.SetActive(true);
+        eventSystem.SetSelectedGameObject(returnButton);
+    }
+    public void backToMainMenu() 
+    {
+        creditMenu.SetActive(false);
+        mainMenu.SetActive(true);
+        eventSystem.SetSelectedGameObject(playButton);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ GamejamRimouski/Assets/Scenes/Script/Menu/MenuAnimation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuAnimation : MonoBehaviour
+{
+    private bool rotatingLeft;
+    private bool goingUp;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
