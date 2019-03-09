457df014bb6cd2b045f0cb1a9843f23775c98cc4
FLOFLO <flo>
2019-03-09 00:18:52 -0500

    FEAT : Spawn kid random dans zone joueur + delete quand pas attraper

---
+++ GamejamRimouski/Assets/Scenes/Script/Kid.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kid : MonoBehaviour
+{
+
+    public int delayDeletrePrefab = 3;
+    private ObjectifManager objectifManager;
+    public enum State
+    {
+        Wait,
+        inCourse
+    }
+
+    public State state;
+
+    private void Awake()
+    {
+        objectifManager = GameObject.Find("GameManager").GetComponent<ObjectifManager>();
+    }
+
+    void Start()
+    {
+        state = State.Wait;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (state) {
+            case State.Wait:
+                StartCoroutine(delayBeforeDelete());
+                break;
+            case State.inCourse:
+                break;
+        }
+    }
+
+
+    IEnumerator delayBeforeDelete()
+    {
+        yield return new WaitForSeconds(delayDeletrePrefab);
+        objectifManager.deleteObjectifActive();
+        Destroy(this.gameObject);
+    }
+}
---
+++ GamejamRimouski/Assets/Scenes/Script/ObjectifManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectifManager : MonoBehaviour
+{
+
+    //public List<GameObject> listObjectif;
+    public List<GameObject> listTarget;
+    public GameObject prefabKid;
+    public GameObject hitBoxSpawnObjectif;
+
+
+    private Vector3 center;
+    public Vector3 size;
+
+    // Start is called before the first frame update
+    public int objectifMax = 3;
+    private int objectifActive = 0;
+
+    void Start()
+    {
+        spawnObjectif();
+    }
+
+    private void Update()
+    {
+        if(objectifActive != 3)
+        {
+            spawnObjectif();
+        }
+    }
+
+    private void spawnObjectif()
+    {
+        center = hitBoxSpawnObjectif.transform.position;
+        Vector3 pos = center + new Vector3(Random.Range(-size.x / 2, size.x / 2), 0, Random.Range(-size.z / 2, size.z / 2));
+        Instantiate(prefabKid, pos, Quaternion.identity);
+        objectifActive++;
+    }
+
+    public void deleteObjectifActive()
+    {
+        objectifActive--;
+    }
+}
