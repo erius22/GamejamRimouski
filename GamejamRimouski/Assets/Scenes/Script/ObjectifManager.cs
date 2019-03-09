51134aa089623ccd2e7433b7ea7a27da7c48e50c
Jacob Murray <jaymur2010@gmail.com>
2019-03-09 16:17:50 -0500

    FLECHE

--- GamejamRimouski/Assets/Scenes/Script/ObjectifManager.cs
+++ GamejamRimouski/Assets/Scenes/Script/ObjectifManager.cs
@@ -5,35 +5,58 @@ using UnityEngine;
 public class ObjectifManager : MonoBehaviour
 {
 
+    //public List<GameObject> listObjectif;
     public List<GameObject> listTarget;
     public GameObject prefabKid;
     public GameObject hitBoxSpawnKid;
+    public GameObject player;
+    public GameObject arrow;
 
+
+    private Vector3 targetDirection;
     private GameObject ActiveTarget;
     private Vector3 center;
+    public Vector3 size;
 
 
+    // Start is called before the first frame update
     public int objectifMax = 3;
     private int objectifActive = 0;
 
     void Start()
     {
-        for (int i = 0; i < 3;i++)
+        spawnKid();
+    }
+
+    private void Update()
+    {
+        if(objectifActive != 3)
         {
             spawnKid();
         }
-    }
 
+        if(ActiveTarget != null)
+        {
+            Debug.Log(ActiveTarget.transform.position);
+
+            //targetDirection = ActiveTarget.transform.TransformDirection(ActiveTarget.transform.position - arrow.transform.position);
+            //float angle = Mathf.Atan2(-targetDirection.x, targetDirection.y) * Mathf.Rad2Deg;
+            //arrow.transform.eulerAngles = new Vector3(0, angle);
+            //transform.rotation = Quaternion.Slerp(arrow.transform.rotation, Quaternion.LookRotation(ActiveTarget.transform.position), 2);
+            //float targetAngle = Mathf.Atan2(t_ShootingDirection.y, t_ShootingDirection.x) * Mathf.Rad2Deg;
+            var q = Quaternion.LookRotation(ActiveTarget.transform.position - arrow.transform.position);
+            arrow.transform.rotation = Quaternion.RotateTowards(arrow.transform.rotation, q, 360);
+        }
+    }
     public void setActiveTarget(GameObject target)
     {
         this.ActiveTarget = target;
     }
 
-    public void spawnKid()
+    private void spawnKid()
     {
         center = hitBoxSpawnKid.transform.position;
-        Vector3 sizeHitBoxSpawnKid = new Vector3(hitBoxSpawnKid.transform.position.x, hitBoxSpawnKid.transform.position.y, hitBoxSpawnKid.transform.position.z);
-        Vector3 pos = center + new Vector3(Random.Range(-sizeHitBoxSpawnKid.x / 2, sizeHitBoxSpawnKid.x / 2), Random.Range(-sizeHitBoxSpawnKid.y / 2, sizeHitBoxSpawnKid.y / 2), Random.Range(-sizeHitBoxSpawnKid.z / 2, sizeHitBoxSpawnKid.z / 2));
+        Vector3 pos = center + new Vector3(Random.Range(-size.x / 2, size.x / 2), Random.Range(-size.y / 2, size.y / 2), Random.Range(-size.z / 2, size.z / 2));
         Instantiate(prefabKid, pos, Quaternion.identity);
         objectifActive++;
     }
