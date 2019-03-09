c7f33093b7353116aca602d615562df68531101b
FLOFLO <flolevy33@gmail.com>
2019-03-09 13:39:52 -0500

    featr : debut script AI poisson

---
+++ GamejamRimouski/Assets/Scenes/Script/Fish/AIMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIMove : MonoBehaviour
+{
+
+    private AISpawner m_AIManager;
+
+    private bool m_isMoving = false;
+    private bool m_isTurning;
+
+    private Vector3 m_wayPoint;
+    private Vector3 m_lastWaypoint = new Vector3(0f, 0f, 0f);
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
