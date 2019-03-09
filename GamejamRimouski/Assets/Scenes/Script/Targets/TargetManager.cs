812c8a53a00c73501dd22d4aab5b934ba5405d91
Jacob Murray <jaymur2010@gmail.com>
2019-03-09 18:52:06 -0500

    TargetManager Début

---
+++ GamejamRimouski/Assets/Scenes/Script/Targets/TargetManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetManager : MonoBehaviour
+{
+
+    public GameObject arrow;
+
+    private GameObject activeTarget;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        arrow.SetActive(true);
+        if (activeTarget == null)
+        {
+            arrow.SetActive(false);
+        }
+
+        if (activeTarget != null)
+        {
+            
+            Debug.Log(activeTarget.transform.position);
+
+
+            var q = Quaternion.LookRotation(arrow.transform.position - activeTarget.transform.position);
+            arrow.transform.rotation = Quaternion.RotateTowards(arrow.transform.rotation, q, 360);
+
+        }
+
+        
+    }
+    public void setActiveTarget(GameObject target)
+    {
+        this.activeTarget = target;
+        Debug.Log(activeTarget);
+    }
+}
