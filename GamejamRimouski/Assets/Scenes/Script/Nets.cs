1fdf6856b8ed3ae856c853db6097cab3a71054b1
Jacob Murray <jaymur2010@gmail.com>
2019-03-09 12:31:09 -0500

    2eme scene de test

---
+++ GamejamRimouski/Assets/Scenes/Script/Nets.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nets : MonoBehaviour
+{
+    public GameObject net;
+    public float speed = 5;
+    private bool playerIsNear = false;
+
+
+    private void OnTriggerStay(Collider collider)
+    {
+        if (collider.tag == "player")
+        {
+            playerIsNear = true;
+        }
+        
+    }
+    private void OnTriggerExit(Collider collider)
+    {
+        if(collider.tag == "player")
+        {
+            playerIsNear = false;
+        }
+    }
+
+    void Update()
+    {
+        if (playerIsNear && net.transform.position.y >= 1.75)
+            net.transform.Translate(0, -1 * Time.deltaTime * speed, 0);
+        if(!playerIsNear && net.transform.position.y <= 7)
+            net.transform.Translate(0, 1 * Time.deltaTime * speed, 0);
+    }
+}
