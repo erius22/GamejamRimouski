4be0e909e85e959b471dff2360349763441dfff9
Erius <fortin.eric.24@hotmail.com>
2019-03-09 01:34:49 -0500

    control de base joueur + camera
    accelerer/ralentir/reculer
    
    rotation gauche/droite haut/base
    
    controle camera de base

---
+++ GamejamRimouski/Assets/Scenes/Script/CameraFollow.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Camera camera;
+
+    public Vector3 offset;
+
+    public Transform objectToFollow;
+
+    //public float followSpeed = 10;
+    public float rotationHorizontalMaxAngle = 50;
+    public float rotationVerticalMaxAngle = 20;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (camera == null)
+        {
+            camera = GetComponentInChildren<Camera>();
+        }
+    }
+
+    void Update()
+    {
+        transform.position = objectToFollow.position + offset;
+        transform.rotation = objectToFollow.rotation;
+
+        float xRot = rotationVerticalMaxAngle * Input.GetAxis("VerticalCamera");
+        float yRot = rotationHorizontalMaxAngle * Input.GetAxis("HorizontalCamera");
+
+        if (xRot != 0 || yRot != 0)
+        {
+            transform.Rotate(xRot, yRot, 0.0f);
+        }
+        // Quaternion counterRotationCam = transform.rotation;
+        // counterRotationCam.z = counterRotationCam.z * -1;*/
+
+        //camera.transform.rotation = counterRotationCam;
+        //  camera.transform.rotation = (transform.rotation.z, transform.rotation.y, transform.rotation.z * -1);
+    }
+}
---
+++ GamejamRimouski/Assets/Scenes/Script/ControlPlayer.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ControlPlayer : MonoBehaviour
+{
+
+    [SerializeField]
+    private float forwardSpeedMultiplier = 1;
+    [SerializeField]
+    private float upwardSpeedMultiplier = 1;
+
+    private Rigidbody m_rigidbody;
+    public float maxSpeed = 200f;//Replace with your max speed
+
+    private float forwardSpeed;
+
+    public float horizontalRotationSpeed = 10;
+    public float verticalRotationSpeed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       m_rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        forwardSpeed = Input.GetAxis("Accelerate");
+
+        float movementUpward = Input.GetAxis("Upward");
+
+
+        movementUpward = movementUpward * upwardSpeedMultiplier;
+        forwardSpeed = forwardSpeed * forwardSpeedMultiplier;
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Debug.Log(transform.forward);
+
+
+        //transform.localEulerAngles = new Vector3(0, horizontal * horizontalRotationSpeed, 0);
+
+        transform.Rotate(0, horizontal * horizontalRotationSpeed, 0f);
+        transform.Rotate(vertical * verticalRotationSpeed, 0, 0f);
+       // transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0f);
+
+        
+        transform.RotateAround(transform.position, Vector3.left, verticalRotationSpeed * vertical);
+        Quaternion q_v = transform.rotation;
+        q_v.eulerAngles = new Vector3(q_v.eulerAngles.x, q_v.eulerAngles.y, 0);
+        transform.rotation = q_v;
+
+        transform.RotateAround(transform.position, Vector3.up, horizontalRotationSpeed * horizontal);
+        Quaternion q_h = transform.rotation;
+        q_h.eulerAngles = new Vector3(q_h.eulerAngles.x, q_h.eulerAngles.y, 0);
+        transform.rotation = q_h;
+        
+        //GetComponent<Rigidbody>().AddForce
+
+        //Vector3 movement = new Vector3(0, forwardSpeed, movementUpward * -1);
+
+
+
+        //transform.Translate(movement * Time.deltaTime);
+
+    }
+
+    private void FixedUpdate()
+    {
+         m_rigidbody.AddForce(transform.forward * forwardSpeed, ForceMode.Acceleration);
+      
+        //transform.Translate(transform.forward * forwardSpeed * Time.deltaTime);
+
+        //m_rigidbody.MovePosition(transform.position + transform.forward * Time.deltaTime);
+
+        
+        if (m_rigidbody.velocity.magnitude > maxSpeed)
+        {
+            m_rigidbody.velocity = m_rigidbody.velocity.normalized * maxSpeed;
+        }
+    }
+}
