6024e39d3036c49ea1cfc779d0ecb89fa405deb3
Jacob Murray <jaymur2010@gmail.com>
2019-03-08 23:13:56 -0500

    MenuScene

---
+++ GamejamRimouski/Assets/Scenes/Script/Score/TEST.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEST : MonoBehaviour
+{
+    public ScoreManager scoreManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreManager = FindObjectOfType<ScoreManager>();
+
+    }
+
+    private void Update()
+    {
+
+        scoreManager.AddScore(5);
+    }
+}
