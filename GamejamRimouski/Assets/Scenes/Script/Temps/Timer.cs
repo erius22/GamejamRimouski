9134d8760b82cb79580f342da686e14b7538e463
Jacob Murray <jaymur2010@gmail.com>
2019-03-08 22:09:39 -0500

    Timer Simple + Fonction AddTime

---
+++ GamejamRimouski/Assets/Scenes/Script/Temps/Timer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float time;
+    public Text timeText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 45;
+        timeText.text = time.ToString();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+        timeText.text = time.ToString();
+
+        if(time <= 0)
+        {
+            //TODO GameOver
+        }
+        
+    }
+    
+    public void AddTime(int addedTime)
+    {
+        time += addedTime;
+    }
+}
